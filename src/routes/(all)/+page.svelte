5684b73dc7a51c265897cc7ca98ceba3e52f04c2
mariia-shcherbak <masha.shcherbak.2016@gmail.com>
2023-09-09 16:21:19 +0300

    Update +page.svelte

--- src/routes/(all)/+page.svelte
+++ src/routes/(all)/+page.svelte
@@ -32,45 +32,52 @@
 
 
 {#if data.shopData.topBanner != null}
-<div class="w-full px-4">
+<div class="pb-12 mb-20">
+<div class="w-full px-12">
   <img
     class="w-full h-auto mx-auto"
     src="{data.shopData.topBanner}"
     alt="Banner"
   />
 </div>
+</div>
 {/if}
 
-<div class="w-full relative mt-20">
-  <h2 class="text-2xl font-bold text-gray-600 pl-4 top-0">ПОПУЛЯРНІ ПРОДУКТИ</h2>
-  <a href="#" class="absolute top-0 right-0 mr-4  text-gray-600">
-    <span class="font-bold">Більше  </span>&#129125;
-  </a>
-  <div class="grid grid-cols-4 gap-4" id="productGrid">
-    {#each products.slice(0, 4) as product}
+<div class="">
+<div class="w-full relative">
+  <h2 class="font-inter text-5xl font-extrabold leading-12 tracking-wide text-left text-gray-700 ml-12 mb-4">ПОПУЛЯРНІ ПРОДУКТИ</h2>
+ <a href="#" class="absolute top-9 right-0 mr-12 flex items-center">
+  <span class="font-inter text-lg font-semibold leading-5 tracking-normal text-left text-gray-600">Більше&nbsp;&nbsp;</span>
+  <span class="text-2xl font-thin text-zinc-500">&#129125;</span>
+</a>
+
+
+  <div class="grid grid-cols-4 gap-4 mx-10" id="productGrid">
+    {#each products as product}
       <div class="text-center relative"> 
         {#if product.image}
-          <div class="w-full h-auto pl-4 pr-4 mt-7"> 
+          <div class="w-full h-auto px-2  mt-7 border-rgb-169-167-167 border-1"> 
             <img class="w-full h-auto object-contain border border-gray-300 mb-2" src="{product.image}" alt="Product" /> 
           </div>
         {/if}
-        <div class="text-left pl-4">
-          <h3 class="font-extrabold break-words whitespace-normal text-gray-600">{product.name}</h3>
-          <p class="break-words whitespace-normal text-gray-600">{product.price} грн</p>
+        <div class="text-left pl-2">
+          <h3 class="font-inter font-semibold leading-6 tracking-normal text-gray-700 text-20">{product.name}</h3>
+          <p class="break-words whitespace-normal text-gray-700 text-20">{product.price} грн</p>
         </div>
       </div>
     {/each}
   </div>
+</div>
 </div>
 
 
 <div class="w-full relative overflow-hidden pb-1/3 flex items-center justify-center mt-20" role="region" aria-label="Banner Carousel" on:mouseover={showArrows} on:mouseout={hideArrows} on:focus={showArrows} on:blur={hideArrows}>
   <div class="relative mt-20">
      <div class="flex items-center justify-center">
-  <div class="w-full overflow-x-hidden overflow-y-hidden ml-16 mr-16"> 
+  <div class="w-full overflow-x-hidden overflow-y-hidden mx-12"> 
       <div class="grid grid-cols-2 gap-4">
         {#each banners.slice(startIndexBanners, startIndexBanners + numVisibleBanners) as banner}
-          <div class="product-card my-2 p-2 rounded-md flex flex-col items-center w-full   mb-2"> 
+          <div class="product-card my-2  rounded-md flex flex-col items-center w-full   mb-2"> 
             <img class="max-h-96 object-contain mb-2" src="{banner.url}" alt="Product Banner" /> 
           </div>
         {/each}
@@ -78,38 +85,31 @@
     </div>
     <div>
       </div>
-   <button class="absolute left-0 ml-8 mt-1/2 -translate-y-1/2 focus:outline-none hover:bg-gray-200" on:click={() => startIndexBanners = scrollItems(startIndexBanners, numVisibleBanners, banners, -1)} class:hidden={!arrowsVisible} on:mouseenter={showArrows} on:mouseleave={hideArrows} >&#60;</button>
-    <button class="absolute right-0 mr-8 mt-1/2 -translate-y-1/2 focus:outline-none hover:bg-gray-200" on:click={() => startIndexBanners = scrollItems(startIndexBanners, numVisibleBanners, banners, 1)} class:hidden={!arrowsVisible} on:mouseenter={showArrows} on:mouseleave={hideArrows} >&#62;</button>
+   <button class="absolute left-0 ml-7 mt-1/2 -translate-y-1/2 focus:outline-none hover:bg-gray-200" on:click={() => startIndexBanners = scrollItems(startIndexBanners, numVisibleBanners, banners, -1)} class:hidden={!arrowsVisible} on:mouseenter={showArrows} on:mouseleave={hideArrows} >&#60;</button>
+    <button class="absolute right-0 mr-7 mt-1/2 -translate-y-1/2 focus:outline-none hover:bg-gray-200" on:click={() => startIndexBanners = scrollItems(startIndexBanners, numVisibleBanners, banners, 1)} class:hidden={!arrowsVisible} on:mouseenter={showArrows} on:mouseleave={hideArrows} >&#62;</button>
   </div>
-  
   </div>
 </div>
 
-<div class="flex flex-col items-center mt-8">
-  <div class="categories mt-4">
-    <h2 class="mt-4 font-semibold text-2xl">ПОПУЛЯРНІ КАТЕГОРІЇ :</h2>
-    <ul class="list-none pl-0">
+<div class="flex flex-col my-11 pt-12 ">
+  <div class="categories mt-4 grid grid-cols-2 mx-12">
+    <div class="text-left">
+      <h2 class="font-inter text-5xl font-extrabold leading-40 text-gray-600">ПОПУЛЯРНІ<br>КАТЕГОРІЇ</h2>
+    </div>
+    <ul class="list-none mt-12 col-span-1">
       {#each data.shopData.categories as category, i}
-        <li class="text-gray-600 relative">
-          <div class="my-1"> 
-            <span class="font-semibold italic text-sm">
+        <li class="text-gray-600 relative pl-2">
+          <div class="mt-3"> 
+            <span class=" text-xl  italic leading-24 text-gray-600">
               { i > 9 ? {i} : String(i + 1).padStart(2, '0')}
             </span> 
-            <span class="text-base">{category.name}</span>
+            <span class="font-inter text-2xl font-semibold leading-32 text-gray-600">&nbsp;{category.name}</span>
           </div>
-          <span class="absolute right-0 top-1">&#129125;</span>
-          <div class="w-full border-b border-gray-600 relative">
-          </div>
+          <span class="absolute right-0 top-0 bottom-1 font-inter text-2xl text-gray-500">&#129125;</span>
+          <div class="w-full border-b border-gray-400 relative mt-1"></div>
         </li>
       {/each}
     </ul>
   </div>
 </div>
-
-
-
-
-
-
-
 
