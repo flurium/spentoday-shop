1e6614c92707e6fef1f75e6b92d43e45c009747b
mariia-shcherbak <masha.shcherbak.2016@gmail.com>
2023-09-09 19:44:33 +0300

    Update +page.svelte

--- src/routes/(all)/+page.svelte
+++ src/routes/(all)/+page.svelte
@@ -28,6 +28,8 @@
     }
     return startIndex
   }
+  let phrases = ["Великий вибір", "Новий стиль", "Великий вибір", "Новий стиль", "Великий вибір"]
+  let lastIndex= phrases.length-1;
 </script>
 
 {#if data.shopData.topBanner != null}
@@ -39,10 +41,23 @@
     alt="Banner"
   />
 </div>
+
+<div class="font-inter text-2xl font-semibold leading-8 tracking-wider text-gray-500">
+  <div class="border-t-2 border-b-2 border-gray-400 py-2">
+    {#each phrases as phrase}
+      <div class="mx-8 inline-block uppercase">
+        {phrase}
+      </div>
+      <div class="px-3 inline-block">/</div>
+    {/each}
+  </div>
+</div>
 </div>
 {/if}
 
-<div class="">
+
+
+
 <div class="w-full relative">
   <h2 class="font-inter text-5xl font-extrabold leading-12 tracking-wide text-left text-gray-700 ml-12 mb-4">ПОПУЛЯРНІ ПРОДУКТИ</h2>
  <a href="#" class="absolute top-9 right-0 mr-12 flex items-center">
@@ -53,9 +68,9 @@
 
   <div class="grid grid-cols-4 gap-4 mx-10" id="productGrid">
     {#each products as product}
-      <div class="text-center relative"> 
+      <div class="text-center"> 
         {#if product.image}
-          <div class="w-full h-auto px-2  mt-7 border-rgb-169-167-167 border-1"> 
+          <div class="w-full h-auto px-2 mt-7 border-rgb-169-167-167 border-1"> 
             <img class="w-full h-auto object-contain border border-gray-300 mb-2" src="{product.image}" alt="Product" /> 
           </div>
         {/if}
@@ -67,10 +82,9 @@
     {/each}
   </div>
 </div>
-</div>
 
 <div
-  class="w-full relative overflow-hidden pb-1/3 flex items-center justify-center mt-20"
+  class="w-full overflow-hidden pb-1/3 flex items-center justify-center mt-20"
   role="region"
   aria-label="Banner Carousel"
   on:mouseover={showArrows}
@@ -78,7 +92,7 @@
   on:focus={showArrows}
   on:blur={hideArrows}
 >
-  <div class="relative mt-20">
+  <div class="mt-20">
      <div class="flex items-center justify-center">
   <div class="w-full overflow-x-hidden overflow-y-hidden mx-12"> 
       <div class="grid grid-cols-2 gap-4">
